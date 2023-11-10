0fcaf106a9088870196259bcf05f04d71f8b4d5e
Hamou-PC\Hamou <tryit741852@gmail.com>
2023-11-10 14:39:31 +0800

    finish policyholders api

--- app.js
+++ app.js
@@ -10,7 +10,50 @@ const PORT = env.port || '3000'
 app.use(bodyParser.json())
 
 app.get('/api/policyholders', async (req, res) => {
+    const { code } = req.query
+    if (!code) {
+        return res.status(400).json({ error: 'Invalid Input' })
+    }
+    try {
+        const queryResult = await query('SELECT * FROM invacare_customer WHERE code = ?', [code])
 
+        if (queryResult.length === 0) {
+            return res.status(400).json({ error: 'Invalid Code' })
+        }
+        const result = queryResult[0]
+
+        const { name, registration_date, introducer_code } = result
+
+        const nodeResult = await query('SELECT * FROM invacare_customer WHERE introducer_code = ?', [introducer_code])
+
+        let l = []
+        let r = []
+
+        if (nodeResult.length !== 0) {
+            for (const node of nodeResult) {
+                const { code: nodeCode, name: nodeName, registration_date: nodeDate } = node
+                if (nodeCode === code) continue
+                const nodeObj = {
+                    code: nodeCode,
+                    name: nodeName,
+                    registration_date: nodeDate,
+                    introducer_code
+                }
+                nodeCode > code ? r.push(nodeObj) : l.push(nodeObj)
+            }
+        }
+
+        res.json({
+            code,
+            name,
+            registration_date,
+            introducer_code,
+            l,
+            r
+        })
+    } catch (e) {
+        return res.status(404).json({ error: 'Unknown Error' })
+    }
 })
 
 app.get('/api/policyholders/:code/top', async (req, res) => {
