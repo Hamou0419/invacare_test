e0e40181870511c676f04b02336efae26f692e2c
Hamou-PC\Hamou <tryit741852@gmail.com>
2023-11-10 13:53:14 +0800

    init env

---
+++ env.js
@@ -0,0 +1,11 @@
+const env = {
+    "port": "3000",
+    "mysql": {
+        host: "127.0.0.1",
+        user: "",
+        password: "",
+        database: "invacare",
+    }
+}
+
+module.exports = env
