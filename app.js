138c95e5431f46e41e5adb7d6821a308a776a89f
Hamou-PC\Hamou <tryit741852@gmail.com>
2023-11-10 15:08:58 +0800

    finish api

--- app.js
+++ app.js
@@ -57,7 +57,70 @@ app.get('/api/policyholders', async (req, res) => {
 })
 
 app.get('/api/policyholders/:code/top', async (req, res) => {
+    const { code } = JSON.parse(JSON.stringify(req.params))
+    if (!code) {
+        return res.status(400).json({ error: 'Invalid Input' })
+    }
 
+    try {
+        const queryResult = await query('SELECT * FROM invacare_customer WHERE code = ?', [code])
+
+        if (queryResult.length === 0) {
+            return res.status(400).json({ error: 'Invalid Code' })
+        }
+        const result = queryResult[0]
+
+        const { introducer_code } = result
+
+        const topResult = await query('SELECT * FROM invacare_customer WHERE code = ?', [introducer_code])
+        let root = false
+        let topCode, topName, topDate, topIntroducer
+        if (topResult.length === 0) root = true
+        if (root) {
+            topCode = result.code
+            topName = result.name
+            topDate = result.registration_date
+            topIntroducer = result.introducer_code
+        } else {
+            const top = topResult[0]
+            topCode = top.code
+            topName = top.name
+            topDate = top.registration_date
+            topIntroducer = top.introducer_code
+        }
+
+        let l = []
+        let r = []
+
+        const topNodeResult = await query('SELECT * FROM invacare_customer WHERE introducer_code = ?', [topIntroducer])
+
+
+        if (topNodeResult.length !== 0) {
+            for (const node of topNodeResult) {
+                const { code: nodeCode, name: nodeName, registration_date: nodeDate } = node
+                if (nodeCode === code) continue
+                const nodeObj = {
+                    code: nodeCode,
+                    name: nodeName,
+                    registration_date: nodeDate,
+                    introducer_code
+                }
+                nodeCode > code ? r.push(nodeObj) : l.push(nodeObj)
+            }
+        }
+
+        res.json({
+            code: topCode,
+            name: topName,
+            registration_date: topDate,
+            introducer_code: topIntroducer,
+            l,
+            r
+        })
+    } catch (e) {
+        console.log(e.message)
+        return res.status(404).json({ error: 'Unknown Error' })
+    }
 })
 
 app.listen(PORT, async () => {
